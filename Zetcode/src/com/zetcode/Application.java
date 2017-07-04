151eedf03dea3255c77218a7faf72be16729e6d8
lcc-at <liaocc.at@outlook.com>
2017-07-04 07:18:34 -0300

    Add files via upload

---
+++ Zetcode/src/com/zetcode/Application.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zetcode;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+
+public class Application extends JFrame {
+    
+    public Application() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        add(new Screen());
+
+        setSize(350, 350);
+
+        setTitle("Application");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }    
+    
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Application ex = new Application();
+                ex.setVisible(true);
+            }
+        });
+    }
+}
